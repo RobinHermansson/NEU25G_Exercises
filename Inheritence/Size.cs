90eb584075eec24c386b547dff9bb44833acb1d9
Robin Hermansson <robin.hermansson@stendahls.se>
2025-09-30 07:20:59 +0200

    INHERITENCE 4, solved

---
+++ Inheritence/Size.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    public struct Size
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public int Length { get; set; }
+
+        public Size (int length, int width, int height)
+        {
+
+            Length = length;
+            Width = width;
+            Height = height;
+
+        }
+    }
+}
