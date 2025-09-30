cd3d503ed6108ede0806769175787614d196030c
Robin Hermansson <robin.hermansson@stendahls.se>
2025-09-30 06:45:15 +0200

    Prepared 'Inheriitence' exercises

---
+++ Inheritence/Program.cs
@@ -0,0 +1,48 @@
+﻿
+while (true) 
+{
+    Console.WriteLine("Select any of the given Exercise options " +
+        "\n1. Create a class for vehicles" +
+        "\n2. Autoproperty LastName (NO PRINT AS IT WAS INTERNAL CHANGE)" +
+        "\n3. Create a subclass for Cars" +
+        "\n4. Create a struct to store length, width and height" +
+        "\n5. Initialize Vehicle and print Car" +
+        "\n6. Circles inheritiing from Shape" +
+        "\n7. Squares also inheriting from Shape" +
+        "\n8. Adding Print() to Shapes" +
+        "\n9. An array of circles and squares" +
+        "\n10. Static priint methods" +
+        "\n11. Coloriize 'PrintAll()'" +
+        "\n12. Random colors"
+        );
+
+    int selection = int.Parse(Console.ReadLine());
+
+    switch (selection)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        default:
+            break;
+                 
+    }
+}
+
