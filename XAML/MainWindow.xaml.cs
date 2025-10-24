8263e0b7f13ae847d387604fb53571356880e060
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-24 13:33:23 +0200

    XAML Exercise 1 finished

--- XAML/MainWindow.xaml.cs
+++ XAML/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace XAML
 {
@@ -23,6 +10,14 @@ namespace XAML
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Assignment1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment1 assignment1 = new Assignment1();
+            assignment1.Show();
+
         }
     }
 }
