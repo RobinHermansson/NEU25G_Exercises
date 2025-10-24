71b1791c81daed105735a9c0aeb2ec75702f825e
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-24 13:41:40 +0200

    XAML exercises more prep

--- XAML/MainWindow.xaml.cs
+++ XAML/MainWindow.xaml.cs
@@ -26,5 +26,47 @@ namespace XAML
             assignment2.Show();
 
         }
+        private void Assignment3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment3 assignment3 = new Assignment3();
+            assignment3.Show();
+
+        }
+        private void Assignment4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment4 assignment4 = new Assignment4();
+            assignment4.Show();
+
+        }
+        private void Assignment5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment5 assignment5 = new Assignment5();
+            assignment5.Show();
+
+        }
+        private void Assignment6Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment6 assignment6 = new Assignment6();
+            assignment6.Show();
+
+        }
+        private void Assignment7Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment7 assignment7 = new Assignment7();
+            assignment7.Show();
+
+        }
+        private void Assignment8Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment8 assignment8 = new Assignment8();
+            assignment8.Show();
+
+        }
     }
 }
