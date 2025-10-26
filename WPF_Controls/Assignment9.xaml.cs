6e9b9068d013fbddfff0911ab988db5ceb962bc5
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-26 14:32:56 +0100

    WPF Controller preparations

---
+++ WPF_Controls/Assignment9.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Controls
+{
+    /// <summary>
+    /// Interaction logic for Assignment9.xaml
+    /// </summary>
+    public partial class Assignment9 : Window
+    {
+        public Assignment9()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPF_Controls/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void Assignment1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment1 assignment1 = new Assignment1();
+            assignment1.Show();
+
+        }
+        private void Assignment2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment2 assignment2 = new Assignment2();
+            assignment2.Show();
+
+        }
+        private void Assignment3Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment3 assignment3 = new Assignment3();
+            assignment3.Show();
+
+        }
+        private void Assignment4Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment4 assignment4 = new Assignment4();
+            assignment4.Show();
+
+        }
+        private void Assignment5Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment5 assignment5 = new Assignment5();
+            assignment5.Show();
+
+        }
+        private void Assignment6Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment6 assignment6 = new Assignment6();
+            assignment6.Show();
+
+        }
+        private void Assignment7Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment7 assignment7 = new Assignment7();
+            assignment7.Show();
+
+        }
+        private void Assignment8Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment8 assignment8 = new Assignment8();
+            assignment8.Show();
+
+        }
+        private void Assignment9Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment9 assignment9 = new Assignment9();
+            assignment9.Show();
+
+        }
+        private void Assignment10Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment10 assignment10 = new Assignment10();
+            assignment10.Show();
+
+        }
+        private void Assignment11Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment11 assignment11 = new Assignment11();
+            assignment11.Show();
+
+        }
+        private void Assignment12Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Assignment12 assignment12 = new Assignment12();
+            assignment12.Show();
+
+        }
+    }
+}
