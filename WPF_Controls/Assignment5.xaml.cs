84da136e92d82b056a42918f8b1ec129d2575d13
Robin Hermansson <robin.hermansson@stendahls.se>
2025-10-31 06:30:47 +0100

    WPF_Controls work in progress, exercise 5.

--- WPF_Controls/Assignment5.xaml.cs
+++ WPF_Controls/Assignment5.xaml.cs
@@ -1,27 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Navigation;
+
 
 namespace WPF_Controls
+
 {
     /// <summary>
     /// Interaction logic for Assignment5.xaml
     /// </summary>
     public partial class Assignment5 : Window
     {
+
+        private int yPos = 0;
+        private int xPos = 0;
         public Assignment5()
         {
             InitializeComponent();
+        }
+
+        private void labelMoverSlider_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+
+        }
+
+        private void labelMoverSlider_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+
+        }
+
+        private void labelMoverSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            if ((bool)xLock.IsChecked && (bool)yLock.IsChecked)
+            {
+                return;
+            }
+            else if ((bool)!xLock.IsChecked)
+            {
+                xPos = (int)labelMoverSlider.Value;
+                movingLabel.Margin = new Thickness(xPos, yPos, 0, 0);
+            }
+            else if ((bool)!yLock.IsChecked)
+            {
+                yPos = (int)labelMoverSlider.Value;
+                movingLabel.Margin = new Thickness(xPos, yPos, 0, 0);
+            }
+
         }
     }
 }
